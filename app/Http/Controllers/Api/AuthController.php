54207689276a874ce0ed29d4d6a6a1f473d86d87
Efendyzheng <efendyzheng15@gmail.com>
2024-06-11 09:45:13 +0700

    update

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -47,6 +47,8 @@ class AuthController extends Controller
     //update image profile & face_embedding
     public function updateProfile(Request $request)
     {
+        Log::info($request);
+
         $request->validate([
             'image' => 'required',
             'face_embedding' => 'required',
